0befd30f37d61f595bfd8f49d9e0f5a6f21cce99
Ashfaque Khan <42895491+ashfaquekhan@users.noreply.github.com>
2019-09-26 01:52:08 +0530

    Create pathfinder.py

---
+++ pathfinder.py
@@ -0,0 +1,32 @@
+import os
+import sys
+import RPi.GPIO as  GPIO
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(6, GPIO.IN, pull_up_down=GPIO.PUD_UP) # SELECT_BUTTON-PIN-31
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(27, GPIO.IN, pull_up_down=GPIO.PUD_UP) # UP_ARROW_BUTTON-PIN-13
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(5, GPIO.IN, pull_up_down=GPIO.PUD_UP) # DOWN_ARROW_BUTTON-PIN-29
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(22, GPIO.IN, pull_up_down=GPIO.PUD_UP) # BACK_BUTTON-PIN-15
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(24, GPIO.IN, pull_up_down=GPIO.PUD_UP) # ENTER_BUTTON-PIN-18
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(23, GPIO.IN, pull_up_down=GPIO.PUD_UP) # DELETE_BUTTON-PIN-16
+back =  GPIO.input(27)
+
+# name|go_src|def_src|operation
+
+if __name__== '__main__':
+    name = str(sys.argv[1])+".py"
+    path_src = str(sys.argv[2])
+    os.system("python "+ str(name) +" "+ str(path_src))
+
+elif back == False:
+    os.system('python main_menu.py')
+
