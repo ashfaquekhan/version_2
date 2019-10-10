8c67d54d8938c34f4547f7ed317065610619259f
Ashfaque Khan <42895491+ashfaquekhan@users.noreply.github.com>
2019-10-10 23:06:34 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,7 +7,6 @@ set -e
 sudo apt-get install -y \
   git-core \
   python-setuptools \
-  python-imaging \
   python-dev \
   python-pip
   
@@ -15,18 +14,18 @@ sudo apt-get install -y \
 cd
 git clone https://github.com/Gadgetoid/WiringPi2-Python.git
 cd WiringPi2-Python/
-sudo python setup.py install
+sudo python3 setup.py install
 ./build.sh
 cd ..
 git clone https://github.com/doceme/py-spidev
 cd py-spidev
-sudo python setup.py install
+sudo python3 setup.py install
 cd ..
 git clone https://github.com/guyc/py-gaugette.git
 cd py-gaugette
-sudo python setup.py install
+sudo python3 setup.py install
 cd
-sudo pip install wiringpi
+sudo pip3 install wiringpi
 cd OLED/python-examples
 wget http://www.ralphmag.org/HS/penguins900x600.jpg 
 cd
